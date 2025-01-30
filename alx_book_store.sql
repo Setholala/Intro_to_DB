3ffd56bbf625e491c7bae27c49bc83d5ef92aca6
setholala <Sethedia40@gmail.com>
2025-01-29 17:41:13 -0800

    Design database for an online store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,8 +33,8 @@ order_date DATE
 CREATE TABLE IF NOT EXISTS Order_Details(
 orderdetailid INT PRIMARY KEY,
 order_id INT,
-FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
 book_id INT,
-FOREIGN KEY(book_id) REFERENCES Books(book_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id),
 quantity DOUBLE
 );
