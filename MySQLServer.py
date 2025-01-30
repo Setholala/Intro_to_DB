7d925e87613b211a9d6e2c7e56677eddd3184252
setholala <Sethedia40@gmail.com>
2025-01-29 18:00:30 -0800

    Create database

---
+++ MySQLServer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+try:
+    mydb = mysql.connector.connect(
+    host = "localhost"
+    user = "root"
+    password = "nextvisiontech2022@gmail.comU"
+    )
+except Exception:
+    print("Database connection failed")
+else:
+    print("Database connection successful")
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+print("Database 'alx_book_store' created successfully!")
+
+mycursor.close()
+mydb.close()
