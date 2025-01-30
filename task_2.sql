bba69d9beea97f2c64a937b56967b6924ba7d7c3
setholala <Sethedia40@gmail.com>
2025-01-29 18:57:59 -0800

    Create tables for a database

--- task_2.sql
+++ task_2.sql
@@ -22,15 +22,15 @@ address TEXT
 CREATE TABLE IF NOT EXISTS Orders (
 order_id INT PRIMARY KEY,
 customer_id INT,
-FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 order_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
 orderdetailid INT PRIMARY KEY,
 order_id INT,
-FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
 book_id INT,
-FOREIGN KEY (book_id) REFERENCES Books (book_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id),
 quantity DOUBLE
 );
